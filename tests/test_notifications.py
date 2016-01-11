f1822813506f4c9f6f9c730db7b581b01425d20a
Albert Casals <skarbat@gmail.com>
2016-01-11 20:38:23 +0100

    Adapting tests to run on 1024x768 so they can be automated.

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -61,13 +61,13 @@ class TestNotificationsPythonApi(unittest.TestCase):
             pass
         
         return count
-                
+
     def test_send_one_notification(self):
         self.assertIsNotNone(os.getenv('DISPLAY'))
 
         notifications=api.import_notification_api()
         self.assertTrue(notifications)
-        
+
         # send a notification popup to the desktop
         notifications.display_generic_notification('test_send_one_notification', 'sample test line')
         time.sleep(1)
@@ -76,13 +76,14 @@ class TestNotificationsPythonApi(unittest.TestCase):
         self.assertEqual(self._get_notification_count(), 1, msg='Notification does not seem to popup')
 
         # FIXME: coordinates will not work unless 1980x1080 display
-        rawinput.click(780, 690)
+        rawinput.click(680, 650)
         time.sleep(5)
+        os.system('scrot /tmp/tests_screenshots/one_notification_complete.png')
 
         # FIXME: for some reason the daemon always shows a notification "please remember to register"
         self.assertEqual(self._get_notification_count(), 0, msg='Notification does not disappear')
 
-    @unittest.skipIf('-fast' in sys.argv, 'Skipping because tests are in fast mode')
+    @unittest.skipIf('-fazt' in sys.argv, 'Skipping because tests are in fast mode')
     def test_send_many_notifications(self):
         self.assertIsNotNone(os.getenv('DISPLAY'))
 
@@ -94,15 +95,18 @@ class TestNotificationsPythonApi(unittest.TestCase):
         # send a notification popup to the desktop one after the next
         for i in range(0, num_popups):
             notifications.display_generic_notification('test_send_many_notifications {}'.format(i), 'sample test line')
-            time.sleep(0.5)
+            time.sleep(1)
             
         # click on each stacked notification one by one
         for i in range(0, num_popups):
             self.assertEqual(self._get_notification_count(), 1)
 
             # FIXME: coordinates will not work unless 1980x1080
-            rawinput.click(780, 690)
-            time.sleep(0.5)
+            rawinput.click(715, 650)
+            time.sleep(1)
+
+        time.sleep(5)
+        os.system('scrot /tmp/tests_screenshots/many_notifications_complete.png')
 
         # make sure all notifications popups are gone
         self.assertEqual(self._get_notification_count(), 0, msg='Notifications popups still found')
